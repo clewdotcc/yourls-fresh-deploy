25e452cd9409e8fa2ee6bd7f4c284e6b7a1ab04d
Colin Meulema <colinmeulema@Bigmac2s-MacBook-Pro.local>
2025-05-24 11:42:25 +0200

    Fix: prevent fatal in_array error by defining $yourls_reserved_keywords

--- user/config.php
+++ user/config.php
@@ -33,3 +33,6 @@ define( 'YOURLS_COOKIEKEY', 'xkSYyNwVqfUyKA7u4vC1nmjgMJ86v3eaDd0nFd3cR8FCXPyVDzS
  ** All done. Have fun!
  */
 
+// ✅ FIX: Prevent fatal error from in_array() when checking reserved keywords
+$yourls_reserved_keywords = [];
+

ed26f7c9f45123c47698299aca104600461105db
Colin Meulema <colinmeulema@Bigmac2s-MacBook-Pro.local>
2025-05-25 17:20:53 +0200

    ✅ Restore config.php to user folder for YOURLS

--- user/config.php
+++ user/config.php
@@ -32,5 +32,5 @@ define( 'YOURLS_COOKIEKEY', 'xkSYyNwVqfUyKA7u4vC1nmjgMJ86v3eaDd0nFd3cR8FCXPyVDzS
 // ✅ FIX: Prevent fatal in_array() error
 $yourls_reserved_keywords = [];
 
-// FORCE_REDEPLOY_TRIGGER: LICM_999
+// FORCE_REDEPLOY_TRIGGER: LICM_989
 
